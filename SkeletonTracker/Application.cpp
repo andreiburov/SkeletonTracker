b2a5d9a85c029568226cd878806d18789ab75dec
Andrei Burov <anders.burov@gmail.com>
2018-01-27 18:35:42 +0100

    Added Direct3D device

--- SkeletonTracker/Application.cpp
+++ SkeletonTracker/Application.cpp
@@ -106,7 +106,8 @@ int Application::Run(HINSTANCE hInstance, int nCmdShow)
 
 		// Explicitly check the Kinect frame event since MsgWaitForMultipleObjects
 		// can return for other reasons even though it is signaled.
-		Update();
+		CheckKinectData();
+		RenderSimpleModel();
 
 		if (PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE))
 		{
@@ -121,13 +122,15 @@ int Application::Run(HINSTANCE hInstance, int nCmdShow)
 		}
 	}
 
+	CleanupDevice();
+
 	return static_cast<int>(msg.wParam);
 }
 
 /// <summary>
 /// Main processing function
 /// </summary>
-void Application::Update()
+void Application::CheckKinectData()
 {
 	if (NULL == m_pNuiSensor)
 	{
@@ -190,6 +193,13 @@ LRESULT CALLBACK Application::DlgProc(HWND hWnd, UINT message, WPARAM wParam, LP
 
 		// Init Direct2D
 		D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &m_pD2DFactory);
+
+		// Init Direct3D
+		if (FAILED(InitDevice()))
+		{
+			CleanupDevice();
+			return 0;
+		}
 
 		// Look for a connected Kinect, and create it if found
 		CreateFirstConnected();
@@ -384,6 +394,191 @@ void Application::ProcessSkeleton()
 		hr = S_OK;
 		DiscardDirect2DResources();
 	}
+}
+
+//--------------------------------------------------------------------------------------
+// Create Direct3D device and swap chain
+//--------------------------------------------------------------------------------------
+HRESULT Application::InitDevice()
+{
+	HRESULT hr = S_OK;
+	HWND hWnd = GetDlgItem(m_hWnd, IDC_VIEW_TWO);
+	RECT rc;
+	GetClientRect(hWnd, &rc);
+	UINT width = rc.right - rc.left;
+	UINT height = rc.bottom - rc.top;
+
+	UINT createDeviceFlags = 0;
+#ifdef _DEBUG
+	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+	D3D_DRIVER_TYPE driverTypes[] =
+	{
+		D3D_DRIVER_TYPE_HARDWARE,
+		D3D_DRIVER_TYPE_WARP,
+		D3D_DRIVER_TYPE_REFERENCE,
+	};
+	UINT numDriverTypes = ARRAYSIZE(driverTypes);
+
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0,
+	};
+	UINT numFeatureLevels = ARRAYSIZE(featureLevels);
+
+	for (UINT driverTypeIndex = 0; driverTypeIndex < numDriverTypes; driverTypeIndex++)
+	{
+		D3D_DRIVER_TYPE driverType = driverTypes[driverTypeIndex];
+		hr = D3D11CreateDevice(nullptr, driverType, nullptr, createDeviceFlags, featureLevels, numFeatureLevels,
+			D3D11_SDK_VERSION, &m_pd3dDevice, &m_featureLevel, &m_pImmediateContext);
+
+		if (hr == E_INVALIDARG)
+		{
+			// DirectX 11.0 platforms will not recognize D3D_FEATURE_LEVEL_11_1 so we need to retry without it
+			hr = D3D11CreateDevice(nullptr, driverType, nullptr, createDeviceFlags, &featureLevels[1], numFeatureLevels - 1,
+				D3D11_SDK_VERSION, &m_pd3dDevice, &m_featureLevel, &m_pImmediateContext);
+		}
+
+		if (SUCCEEDED(hr))
+			break;
+	}
+	if (FAILED(hr))
+		return hr;
+
+	// Obtain DXGI factory from device (since we used nullptr for pAdapter above)
+	IDXGIFactory1* dxgiFactory = nullptr;
+	{
+		IDXGIDevice* dxgiDevice = nullptr;
+		hr = m_pd3dDevice->QueryInterface(__uuidof(IDXGIDevice), reinterpret_cast<void**>(&dxgiDevice));
+		if (SUCCEEDED(hr))
+		{
+			IDXGIAdapter* adapter = nullptr;
+			hr = dxgiDevice->GetAdapter(&adapter);
+			if (SUCCEEDED(hr))
+			{
+				hr = adapter->GetParent(__uuidof(IDXGIFactory1), reinterpret_cast<void**>(&dxgiFactory));
+				adapter->Release();
+			}
+			dxgiDevice->Release();
+		}
+	}
+	if (FAILED(hr))
+		return hr;
+
+	// Create swap chain
+	IDXGIFactory2* dxgiFactory2 = nullptr;
+	hr = dxgiFactory->QueryInterface(__uuidof(IDXGIFactory2), reinterpret_cast<void**>(&dxgiFactory2));
+	if (dxgiFactory2)
+	{
+		// DirectX 11.1 or later
+		hr = m_pd3dDevice->QueryInterface(__uuidof(ID3D11Device1), reinterpret_cast<void**>(&m_pd3dDevice1));
+		if (SUCCEEDED(hr))
+		{
+			(void)m_pImmediateContext->QueryInterface(__uuidof(ID3D11DeviceContext1), reinterpret_cast<void**>(&m_pImmediateContext1));
+		}
+
+		DXGI_SWAP_CHAIN_DESC1 sd;
+		ZeroMemory(&sd, sizeof(sd));
+		sd.Width = width;
+		sd.Height = height;
+		sd.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		sd.SampleDesc.Count = 1;
+		sd.SampleDesc.Quality = 0;
+		sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+		sd.BufferCount = 1;
+
+		hr = dxgiFactory2->CreateSwapChainForHwnd(m_pd3dDevice, hWnd, &sd, nullptr, nullptr, &m_pSwapChain1);
+		if (SUCCEEDED(hr))
+		{
+			hr = m_pSwapChain1->QueryInterface(__uuidof(IDXGISwapChain), reinterpret_cast<void**>(&m_pSwapChain));
+		}
+
+		dxgiFactory2->Release();
+	}
+	else
+	{
+		// DirectX 11.0 systems
+		DXGI_SWAP_CHAIN_DESC sd;
+		ZeroMemory(&sd, sizeof(sd));
+		sd.BufferCount = 1;
+		sd.BufferDesc.Width = width;
+		sd.BufferDesc.Height = height;
+		sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		sd.BufferDesc.RefreshRate.Numerator = 60;
+		sd.BufferDesc.RefreshRate.Denominator = 1;
+		sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+		sd.OutputWindow = hWnd;
+		sd.SampleDesc.Count = 1;
+		sd.SampleDesc.Quality = 0;
+		sd.Windowed = TRUE;
+
+		hr = dxgiFactory->CreateSwapChain(m_pd3dDevice, &sd, &m_pSwapChain);
+	}
+
+	// Note this tutorial doesn't handle full-screen swapchains so we block the ALT+ENTER shortcut
+	dxgiFactory->MakeWindowAssociation(hWnd, DXGI_MWA_NO_ALT_ENTER);
+
+	dxgiFactory->Release();
+
+	if (FAILED(hr))
+		return hr;
+
+	// Create a render target view
+	ID3D11Texture2D* pBackBuffer = nullptr;
+	hr = m_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<void**>(&pBackBuffer));
+	if (FAILED(hr))
+		return hr;
+
+	hr = m_pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &m_pRenderTargetView);
+	pBackBuffer->Release();
+	if (FAILED(hr))
+		return hr;
+
+	m_pImmediateContext->OMSetRenderTargets(1, &m_pRenderTargetView, nullptr);
+
+	// Setup the viewport
+	D3D11_VIEWPORT vp;
+	vp.Width = (FLOAT)width;
+	vp.Height = (FLOAT)height;
+	vp.MinDepth = 0.0f;
+	vp.MaxDepth = 1.0f;
+	vp.TopLeftX = 0;
+	vp.TopLeftY = 0;
+	m_pImmediateContext->RSSetViewports(1, &vp);
+
+	return S_OK;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Render the frame
+//--------------------------------------------------------------------------------------
+void Application::RenderSimpleModel()
+{
+	// Just clear the backbuffer
+	m_pImmediateContext->ClearRenderTargetView(m_pRenderTargetView, DirectX::Colors::MidnightBlue);
+	m_pSwapChain->Present(0, 0);
+}
+
+
+//--------------------------------------------------------------------------------------
+// Clean up the objects we've created
+//--------------------------------------------------------------------------------------
+void Application::CleanupDevice()
+{
+	if (m_pImmediateContext) m_pImmediateContext->ClearState();
+
+	if (m_pRenderTargetView) m_pRenderTargetView->Release();
+	if (m_pSwapChain1) m_pSwapChain1->Release();
+	if (m_pSwapChain) m_pSwapChain->Release();
+	if (m_pImmediateContext1) m_pImmediateContext1->Release();
+	if (m_pImmediateContext) m_pImmediateContext->Release();
+	if (m_pd3dDevice1) m_pd3dDevice1->Release();
+	if (m_pd3dDevice) m_pd3dDevice->Release();
 }
 
 /// <summary>
--- SkeletonTracker/Application.h
+++ SkeletonTracker/Application.h
@@ -59,6 +59,16 @@ private:
 
 	// Direct2D
 	ID2D1Factory*            m_pD2DFactory;
+	
+	// Direct3D
+	D3D_FEATURE_LEVEL        m_featureLevel = D3D_FEATURE_LEVEL_11_0;
+	ID3D11Device*            m_pd3dDevice = nullptr;
+	ID3D11Device1*           m_pd3dDevice1 = nullptr;
+	ID3D11DeviceContext*     m_pImmediateContext = nullptr;
+	ID3D11DeviceContext1*    m_pImmediateContext1 = nullptr;
+	IDXGISwapChain*          m_pSwapChain = nullptr;
+	IDXGISwapChain1*         m_pSwapChain1 = nullptr;
+	ID3D11RenderTargetView*  m_pRenderTargetView = nullptr;
 
 	HANDLE                   m_hNextSkeletonEvent;
 
@@ -66,20 +76,22 @@ private:
 	SimpleSkeleton			 m_SimpleSkeleton;
 
 	/// <summary>
-	/// Main processing function
-	/// </summary>
-	void                    Update();
-
-	/// <summary>
 	/// Create the first connected Kinect found 
 	/// </summary>
 	/// <returns>S_OK on success, otherwise failure code</returns>
 	HRESULT                 CreateFirstConnected();
 
+	void                    CheckKinectData();
+
 	/// <summary>
 	/// Handle new skeleton data
 	/// </summary>
 	void                    ProcessSkeleton();
+	
+	// Direct3D
+	HRESULT					InitDevice();
+	void					CleanupDevice();
+	void					RenderSimpleModel();
 
 	/// <summary>
 	/// Ensure necessary Direct2d resources are created
